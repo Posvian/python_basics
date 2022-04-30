efd3abb1471e0553365f380bb5d3fdc0cb2acebb
Posvian <posvianski@mail.ru>
2022-04-30 15:26:49 +0300

    upgrade task_4_04

--- lesson_4/task_4_04.py
+++ lesson_4/task_4_04.py
@@ -1,3 +1,8 @@
+# Написать свой модуль utils и перенести в него функцию
+# currency_rates и currency_rates_advanced, если вы
+# решали задание 2. Создать скрипт, импортировать в
+# него модуль utils и выполнить несколько вызовов функции
+# currency_rates. Убедиться, что ничего лишнего не происходит.
 from utils import currency_rates_advanced, currency_rates
 
 print(currency_rates('http://www.cbr.ru/scripts/XML_daily.asp', "USd"))
