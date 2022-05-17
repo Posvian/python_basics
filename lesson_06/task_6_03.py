b30ac450a11613108cbf6a6718f433ed441618b5
Posvian <posvianski@mail.ru>
2022-05-17 18:55:26 +0300

    remade task_6_03 in both versions

--- lesson_06/task_6_03.py
+++ lesson_06/task_6_03.py
@@ -6,26 +6,43 @@
 # файл. Проверить сохранённые данные.
 
 from os.path import join
+from sys import exit
 
 path1 = join('.', 'files', 'task_3_users.csv')
 path2 = join('.', 'files', 'task_3_hobby.csv')
 path3 = join('.', 'files', 'task_3_rezult.txt')
 rezult = {}
-with open(path1, mode='rt', encoding='utf-8') as f:
-    with open(path2, mode='rt', encoding='utf-8') as f2:
-        for line in f:
-            line = line.strip().split(',')
-            line1 = f2.readline().replace(',', ';').strip()
-            if len(line1) == 0:
-                line1 = None
-            key = line[0][0] + line[1][0] + line[2][0]
-            rezult[key] = line1
 
-with open(path3, mode='wt', encoding='utf-8') as f3:
+
+def dict_write():
     f3.write('{')
     for key, value in rezult.items():
         f3.write(f"'{key}': '{value}', ")
     f3.write('}')
 
 
-
+with open(path1, mode='rt', encoding='utf-8') as f:
+    with open(path2, mode='rt', encoding='utf-8') as f2:
+        with open(path3, mode='wt', encoding='utf-8') as f3:
+            users = f.readlines()
+            hobbies = f2.readlines()
+            i = 0
+            if len(users) >= len(hobbies):
+                while i < len(users):
+                    user = users[i].strip().split(',')
+                    hobby = hobbies[i].strip().replace(',', ';')
+                    if len(hobby[i]) == 0:
+                        hobby = None
+                    dict_key = user[0][0] + user[1][0] + user[2][0]
+                    rezult[dict_key] = hobby
+                    i += 1
+                dict_write()
+            else:
+                while i < len(users):
+                    user = users[i].strip().split(',')
+                    hobby = hobbies[i].strip().replace(',', ';')
+                    dict_key = user[0][0] + user[1][0] + user[2][0]
+                    rezult[dict_key] = hobby
+                    i += 1
+                dict_write()
+                exit(1)
--- lesson_06/task_6_03_01.py
+++ lesson_06/task_6_03_01.py
@@ -16,17 +16,35 @@ path1 = join('.', 'files', 'task_3_users.csv')
 path2 = join('.', 'files', 'task_3_hobby.csv')
 path3 = join('.', 'files', 'task_3_rezult.json')
 rezult = {}
-with open(path1, mode='rt', encoding='utf-8') as f:
-    with open(path2, mode='rt', encoding='utf-8') as f2:
-        for line in f:
-            line = line.strip().split(',')
-            line1 = f2.readline().replace(',', ';').strip()
-            if len(line1) == 0:
-                line1 = None
-            key = line[0][0] + line[1][0] + line[2][0]
-            rezult[key] = line1
 
-rezult_str = json.dumps(rezult, ensure_ascii=False)
-with open(path3, mode='wt', encoding='utf-8') as f3:
+
+def dict_write():
+    rezult_str = json.dumps(rezult, ensure_ascii=False)
     f3.write(rezult_str)
 
+
+with open(path1, mode='rt', encoding='utf-8') as f:
+    with open(path2, mode='rt', encoding='utf-8') as f2:
+        with open(path3, mode='wt', encoding='utf-8') as f3:
+            users = f.readlines()
+            hobbies = f2.readlines()
+            i = 0
+            if len(users) >= len(hobbies):
+                while i < len(users):
+                    user = users[i].strip().split(',')
+                    hobby = hobbies[i].strip().replace(',', ';')
+                    if len(hobby[i]) == 0:
+                        hobby = None
+                    key = user[0][0] + user[1][0] + user[2][0]
+                    rezult[key] = hobby
+                    i += 1
+                dict_write()
+            else:
+                while i < len(users):
+                    user = users[i].strip().split(',')
+                    hobby = hobbies[i].strip().replace(',', ';')
+                    key = user[0][0] + user[1][0] + user[2][0]
+                    rezult[key] = hobby
+                    i += 1
+                dict_write()
+                exit(1)
